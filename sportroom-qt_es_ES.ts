c6fc568211cd889dfe2a099865a13229122467f0
Xiaoyuan Yang <>
2020-02-26 11:20:53 +0100

    translate

--- sportroom-qt_es_ES.ts
+++ sportroom-qt_es_ES.ts
@@ -1,3 +1,283 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="sportroom-qt_es_ES"></TS>
+<TS version="2.1" language="sportroom-qt_es_ES">
+<context>
+    <name>CurrentMatchResultDialog</name>
+    <message>
+        <location filename="currentmatchresultdialog.ui" line="14"/>
+        <source>Current Result</source>
+        <translation type="unfinished">Resultado Actual</translation>
+    </message>
+</context>
+<context>
+    <name>CurrentMatchResultForm</name>
+    <message>
+        <location filename="currentmatchresultform.ui" line="32"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FullMatchResultDialog</name>
+    <message>
+        <location filename="fullmatchresultdialog.ui" line="14"/>
+        <source>Full Result</source>
+        <translation type="unfinished">Todo el Resultado</translation>
+    </message>
+</context>
+<context>
+    <name>FullMatchResultForm</name>
+    <message>
+        <location filename="fullmatchresultform.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.ui" line="162"/>
+        <source>TeamAName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.ui" line="195"/>
+        <location filename="fullmatchresultform.ui" line="281"/>
+        <source>14</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.ui" line="245"/>
+        <source>TeamBName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.ui" line="342"/>
+        <source>13/12/2020</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.cpp" line="160"/>
+        <source>Images (*.png *.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Sport Room</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <source>New Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>Match Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="108"/>
+        <source>Team Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>Full Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source>Teams/Players</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="190"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Import Information&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="193"/>
+        <source>Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="210"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Export Information&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="213"/>
+        <source>Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="450"/>
+        <location filename="mainwindow.ui" line="597"/>
+        <source>T</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="660"/>
+        <location filename="mainwindow.ui" line="714"/>
+        <source>Logo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="747"/>
+        <location filename="mainwindow.ui" line="755"/>
+        <source>Clubs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="760"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <source>Player Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="149"/>
+        <source>Player Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="248"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="248"/>
+        <source>Can you confirm to Reset the Current Match?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="280"/>
+        <location filename="mainwindow.cpp" line="294"/>
+        <source>Images (*.png *.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="313"/>
+        <location filename="mainwindow.cpp" line="333"/>
+        <source>Json (*.json)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MatchStatus</name>
+    <message>
+        <location filename="matchstatus.cpp" line="367"/>
+        <source>J1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchstatus.cpp" line="369"/>
+        <source>J2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchstatus.cpp" line="371"/>
+        <source>J3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchstatus.cpp" line="373"/>
+        <source>J4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchstatus.cpp" line="375"/>
+        <source>J5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchstatus.cpp" line="377"/>
+        <source>JOCS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="matchstatus.cpp" line="379"/>
+        <source>TOTAL</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="fullmatchresultform.cpp" line="75"/>
+        <source>Change Top Left Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.cpp" line="83"/>
+        <source>Change Top Right Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.cpp" line="91"/>
+        <source>Change Bottom Left Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fullmatchresultform.cpp" line="99"/>
+        <source>Change Bottom Right Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScoreboardForm</name>
+    <message>
+        <location filename="scoreboardform.ui" line="26"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="scoreboardform.ui" line="64"/>
+        <source>PlayerA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="scoreboardform.ui" line="117"/>
+        <location filename="scoreboardform.ui" line="453"/>
+        <source>T</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="scoreboardform.ui" line="180"/>
+        <location filename="scoreboardform.ui" line="396"/>
+        <source>13</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="scoreboardform.ui" line="234"/>
+        <location filename="scoreboardform.ui" line="327"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="scoreboardform.ui" line="280"/>
+        <source>PlayerB</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TeamResultDialog</name>
+    <message>
+        <location filename="teamresultdialog.ui" line="14"/>
+        <source>Team Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TeamResultForm</name>
+    <message>
+        <location filename="teamresultform.ui" line="32"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
